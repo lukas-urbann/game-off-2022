0cc96fbaaf7712d71c00f32865729ccd6d75f16b
Lukáš Urban <lukas.urbann@seznam.cz>
2022-11-05 23:27:56 +0100

    added sfx, buttons interact with cursor, switched input system

---
+++ Game Off 2022 Project/Assets/Scripts/Game/UI/Button.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Game.UI
+{
+    public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+    {
+        [SerializeField] private Color
+            baseColor = new Color(1, 1, 1, 1),
+            hoverColor = new Color(0.42f, 0.8f, 0.56f, 1),
+            clickColor = new Color(0.9f,0.82f,0.16f, 1);
+        
+        [SerializeField] private TMP_Text text;
+
+        private void Start()
+        {
+            if(text == null)
+                text = GetComponentInChildren(typeof(TMP_Text), true) as TMP_Text;
+                
+            text.color = baseColor;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            text.color = hoverColor;
+        }
+        
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            text.color = baseColor;
+        }
+        
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            text.color = clickColor;
+        }
+        
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            text.color = baseColor;
+        }
+    }
+}
