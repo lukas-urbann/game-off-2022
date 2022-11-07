89713c520570b9e8151de352e3d8ae7d4683c798
Lukáš Urban <lukas.urbann@seznam.cz>
2022-11-07 14:43:50 +0100

    Added flashing lights

---
+++ Game Off 2022 Project/Assets/Scripts/Lighting/LightFlash.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Lighting
+{
+    public class LightFlash : MonoBehaviour
+    {
+        public float minTimer, maxTimer;
+        public float flashIntensity;
+        private float flashTimer;
+        private List<int> litUp;
+        [SerializeField] private List<Light> lights = new List<Light>();
+
+        private void Start()
+        {
+            StartCoroutine(RefreshTimer());
+            
+            litUp = new List<int>(new int[lights.Count]);
+        }
+
+        private IEnumerator RefreshTimer()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(Random.Range(minTimer, maxTimer));
+                FlashLights();
+            }
+        }
+
+        private void FlashLights()
+        {
+            foreach (Light light in lights)
+            {
+                if (litUp[lights.IndexOf(light)] == 0)
+                {
+                    light.intensity += flashIntensity;
+                    litUp[lights.IndexOf(light)] = 1;
+                }
+                else
+                {
+                    light.intensity -= flashIntensity;
+                    litUp[lights.IndexOf(light)] = 0;
+                }
+            }
+        }
+    }
+}
