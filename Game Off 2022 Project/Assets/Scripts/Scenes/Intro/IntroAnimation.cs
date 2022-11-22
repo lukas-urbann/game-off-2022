db06c9879388e860b9b2c0d7c37160312f228117
Lukáš Urban <lukas.urbann@seznam.cz>
2022-11-22 12:41:06 +0100

    intro complete

---
+++ Game Off 2022 Project/Assets/Scripts/Scenes/Intro/IntroAnimation.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scenes.Intro
+{
+    public class IntroAnimation : MonoBehaviour
+    {
+        [SerializeField] private int animatorIndex = 0;
+        [SerializeField] private Animator[] anims;
+
+        private void OnEnable()
+        {
+            foreach (Animator anim in anims)
+                anim.enabled = false;
+        }
+
+        public void TriggerNextAnimator()
+        {
+            anims[animatorIndex].enabled = true;
+            animatorIndex++;
+        }
+    }
+}
