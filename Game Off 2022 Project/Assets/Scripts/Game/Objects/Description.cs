9d21f3d8b6ad8782cf90b8b2f362785079c084ae
KackaJeTHICC <61690401+KackaJeTHICC@users.noreply.github.com>
2022-11-28 22:50:05 +0100

    level 4 done
    apart from cutsecne :skull:

--- Game Off 2022 Project/Assets/Scripts/Game/Objects/Description.cs
+++ Game Off 2022 Project/Assets/Scripts/Game/Objects/Description.cs
@@ -3,4 +3,12 @@ using UnityEngine;
 public class Description : MonoBehaviour
 {
     public string description = "Description is not filled in yet!";
+
+    private void Awake()
+    {
+        if (!gameObject.CompareTag("Object"))
+        {
+            Debug.LogWarning(gameObject.name + " does not have tag Object");
+        }
+    }
 }
