b97444a1281eb6b1c31ffd2d07a8f069671c3dad
Lukáš Urban <lukas.urbann@seznam.cz>
2022-12-01 15:33:43 +0100

    endings

--- Game Off 2022 Project/Assets/Scripts/Lukas/Scene/Good Ending/EndingCamera.cs
+++ Game Off 2022 Project/Assets/Scripts/Lukas/Scene/Good Ending/EndingCamera.cs
@@ -7,11 +7,16 @@ namespace Lukas.Scene.GoodEnding
 {
     public class EndingCamera : MonoBehaviour
     {
-        public AudioClip say;
+        public AudioClip say, metalPipe;
         
         public void PlaySay()
         {
             SFXController.Instance.PlaySoundEffectOneShot(say);
+        }
+        
+        public void PlayMetalPipe()
+        {
+            SFXController.Instance.PlaySoundEffectOneShot(metalPipe);
         }
         
     }
